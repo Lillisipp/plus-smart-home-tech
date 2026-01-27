0abe4f230aed384145170412d1d8950b764ced46
Lillisip <Lillisip@yandex.ru>
2026-01-27 18:02:05 +0300

    aggregator добавлен

---
+++ telemetry/aggregator/src/main/java/ru/yandex/practicum/Aggregator.java
@@ -0,0 +1,21 @@
+package ru.yandex.practicum;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.ConfigurableApplicationContext;
+import ru.yandex.practicum.service.AggregationStarter;
+
+@SpringBootApplication
+@ConfigurationPropertiesScan
+public class Aggregator {
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(Aggregator.class, args);
+        AggregationStarter starter = context.getBean(AggregationStarter.class);
+        starter.start();
+
+    }
+}
