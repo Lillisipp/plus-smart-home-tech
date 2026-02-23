44ecdd0f2bf089bd28c2eb6f5d60c5c5679879f2
Lillisip <Lillisip@yandex.ru>
2026-02-23 22:57:48 +0300

    +

--- commerce/shopping-cart/src/main/java/ru/yandex/practicum/controller/ShoppingCartController.java
+++ commerce/shopping-cart/src/main/java/ru/yandex/practicum/controller/ShoppingCartController.java
@@ -1,7 +1,11 @@
 package ru.yandex.practicum.controller;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ru.yandex.practicum.commerce.cart.controller.ShoppingCartApi;
 import ru.yandex.practicum.commerce.cart.dto.ChangeProductQuantityRequest;
@@ -20,31 +24,32 @@ public class ShoppingCartController implements ShoppingCartApi {
     private final CartService shoppingCartService;
 
     @Override
-    public ShoppingCartDto getShoppingCart(String username) {
+    public ShoppingCartDto getShoppingCart(@RequestParam @NotEmpty String username) {
         log.info("ENTER controller getShoppingCart: username={}", username);
         return shoppingCartService.getShoppingCart(username);
     }
 
     @Override
-    public ShoppingCartDto addProductToShoppingCart(String username, Map<UUID, Long> products) {
+    public ShoppingCartDto addProductToShoppingCart(@RequestParam @NotEmpty String username, @RequestBody Map<UUID, Long> products) {
         log.info("ENTER controller addProductToShoppingCart: username={}", username);
         return shoppingCartService.addProductToShoppingCart(username, products);
     }
 
     @Override
-    public void deleteShoppingCart(String username) {
+    public void deleteShoppingCart(@RequestParam @NotEmpty String username) {
         log.info("ENTER controller deactivateCurrentShoppingCart: username={}", username);
         shoppingCartService.deactivateCurrentShoppingCart(username);
     }
 
     @Override
-    public ShoppingCartDto removeProductFromShoppingCart(String username, List<UUID> productIds) {
+    public ShoppingCartDto removeProductFromShoppingCart(@RequestParam @NotEmpty String username,
+                                                         @RequestBody List<UUID> productIds) {
         log.info("ENTER controller removeFromShoppingCart: username={}", username);
         return shoppingCartService.removeFromShoppingCart(username, productIds);
     }
 
     @Override
-    public ShoppingCartDto changeProductQuantity(String username, ChangeProductQuantityRequest request) {
+    public ShoppingCartDto changeProductQuantity(@RequestParam @NotEmpty String username, @RequestBody @Valid ChangeProductQuantityRequest request) {
         log.info("ENTER controller changeProductQuantity: username={}", username);
         return shoppingCartService.changeProductQuantity(username, request);
     }
